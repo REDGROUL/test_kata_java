7f902ffcfdaacad9aba3652d8077ee3d000fd5df
redgroul <redgroulman@gmail.com>
2023-10-28 20:02:52 +0300

    Переработка

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
@@ -15,50 +13,34 @@ public class Main {
 
 
 
-      String s = calculate(input);
-        System.out.println(s);
-
-
-
-
-
 
     }
 
     public static String calculate(String input) {
-        String regex = "(\\d+)\\s*([+\\-*/])\\s*(\\d+)";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(input);
 
-        if (matcher.find()) {
-
-            String operator = matcher.group(2);
-
-            int first = Integer.parseInt(matcher.group(1));
-            int second = Integer.parseInt(matcher.group(3));
-            int res = 0;
-            switch (operator)
-            {
-                case "+":
-                    res = first+second;
-                    break;
-                case "-":
-                    res = first-second;
-                    break;
-                case "/":
-                    res = first/second;
-                    break;
-                case "*":
-                    res = first*second;
-                    break;
-
-            }
-
+        String[] elems = input.split("[+\\-*/]");
+        String num1 = elems[0].trim();
+        String num2 = elems[1].trim();
+        char operator = input.charAt(num1.length());
+        int res = 0;
+        if (operator == '+') {
+            res = Integer.parseInt(num1) + Integer.parseInt(num2);
             return Integer.toString(res);
-
         }
+        if (operator == '-') {
+            res = Integer.parseInt(num1) - Integer.parseInt(num2);
+            return Integer.toString(res);
+        }
+        if (operator == '*') {
+            res = Integer.parseInt(num1) * Integer.parseInt(num2);
+            return Integer.toString(res);
+        }
+        if (operator == '/') {
+            res = Integer.parseInt(num1) / Integer.parseInt(num2);
+            return Integer.toString(res);
+        }
+        throw new IllegalStateException("Unexpected value: " + operator);
 
-        throw new IllegalArgumentException("Invalid input format");
     }
 
 
